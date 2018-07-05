c83cb2e0f32960e1450dd6d25605ec577654272c
myzcxhh@live.cn <imagerty00>
2018-07-05 18:18:45 +0800

    6.5.1 pub

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ BaseFramework框架是我对之前编程开发的一些总结，目的是以最�
 除此之外BaseActivity还提供沉浸式适配，您可以查看Demo的源代码来了解更多。
 
 <a href="https://github.com/kongzue/BaseFramework/">
-<img src="https://img.shields.io/badge/BaseFramework-6.5.0-green.svg" alt="Kongzue BaseFramework">
+<img src="https://img.shields.io/badge/BaseFramework-6.5.1-green.svg" alt="Kongzue BaseFramework">
 </a> 
-<a href="https://bintray.com/myzchh/maven/BaseFramework/6.5.0/link">
-<img src="https://img.shields.io/badge/Maven-6.5.0-blue.svg" alt="Maven">
+<a href="https://bintray.com/myzchh/maven/BaseFramework/6.5.1/link">
+<img src="https://img.shields.io/badge/Maven-6.5.1-blue.svg" alt="Maven">
 </a> 
 <a href="http://www.apache.org/licenses/LICENSE-2.0">
 <img src="https://img.shields.io/badge/License-Apache%202.0-red.svg" alt="License">
@@ -50,14 +50,14 @@ Maven仓库：
 <dependency>
   <groupId>com.kongzue.baseframework</groupId>
   <artifactId>baseframework</artifactId>
-  <version>6.5.0</version>
+  <version>6.5.1</version>
   <type>pom</type>
 </dependency>
 ```
 Gradle：
 在dependencies{}中添加引用：
 ```
-implementation 'com.kongzue.baseframework:baseframework:6.5.0'
+implementation 'com.kongzue.baseframework:baseframework:6.5.1'
 ```
 
 ## BaseActivity功能
@@ -143,7 +143,7 @@ requestPermission(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifes
 
 toast(Obj);
 
-//简易Log打印日志（可通过BaseActivity.DEBUGMODE = false关闭）：
+//简易Log打印日志（可通过BaseActivity.DEBUGMODE = false关闭，注意此开关是同时影响 BaseActivity 和 BaseFragment的）：
 
 log(Obj);
 
@@ -392,6 +392,9 @@ list.setAdapter(baseAdapter);
 ```
 
 ## 更新日志：
+v6.5.0:
+- 修复 BaseFragment 中日志 log 打印不受 BaseActivity.DEBUGMODE 控制的问题；
+
 v6.5.0:
 - 跳转参数 JumpParameter 新增 getBoolean、getInt 和 getString 三个基础方法，从此方法获取数据不需要判断是否为空（null）以及进行强转类型；
 
--- baseframework/src/main/java/com/kongzue/baseframework/BaseFragment.java
+++ baseframework/src/main/java/com/kongzue/baseframework/BaseFragment.java
@@ -17,29 +17,31 @@ import com.kongzue.baseframework.util.JumpParameter;
 import com.kongzue.baseframework.util.OnPermissionResponseListener;
 import com.kongzue.baseframework.util.OnResponseListener;
 
+import static com.kongzue.baseframework.BaseActivity.DEBUGMODE;
+
 /**
- * @Version:    6.4.5
- * @Author:     Kongzue
- * @github:     https://github.com/kongzue/BaseFramework
- * @link:       http://kongzue.com/
- * @describe:   自动化代码流水线作业，以及对原生安卓、MIUI、flyme的透明状态栏显示灰色图标文字的支持，同时提供一些小工具简化开发难度，详细说明文档：https://github.com/kongzue/BaseFramework
+ * @Version: 6.4.5
+ * @Author: Kongzue
+ * @github: https://github.com/kongzue/BaseFramework
+ * @link: http://kongzue.com/
+ * @describe: 自动化代码流水线作业，以及对原生安卓、MIUI、flyme的透明状态栏显示灰色图标文字的支持，同时提供一些小工具简化开发难度，详细说明文档：https://github.com/kongzue/BaseFramework
  */
 
 public abstract class BaseFragment extends Fragment {
-
+    
     public int layoutResId = -1;
-
+    
     public BaseActivity me;
-
+    
     public void setActivity(BaseActivity activity) {
         this.me = activity;
     }
-
+    
     public BaseFragment() {
     }
-
+    
     public View rootView;       //根布局，可以直接用
-
+    
     @Deprecated
     @Nullable
     @Override
@@ -65,36 +67,36 @@ public abstract class BaseFragment extends Fragment {
         setEvents();
         return rootView;
     }
-
+    
     //不再推荐使用，建议直接在Fragment上注解：@Layout(你的layout资源id)
     @Deprecated
     public int getLayout() {
         return layoutResId;
     }
-
+    
     public abstract void initViews();
-
+    
     public abstract void initDatas();
-
+    
     public abstract void setEvents();
-
+    
     public final <T extends View> T findViewById(@IdRes int id) {
         return rootView.findViewById(id);
     }
-
+    
     private Toast toast;
-
+    
     protected void runOnMain(Runnable runnable) {
         me.runOnUiThread(runnable);
     }
-
+    
     protected final static String NULL = "";
-
+    
     //简易吐司
     public void toast(final Object obj) {
         try {
             runOnMain(new Runnable() {
-
+                
                 @Override
                 public void run() {
                     if (toast == null)
@@ -107,19 +109,11 @@ public abstract class BaseFragment extends Fragment {
             e.printStackTrace();
         }
     }
-
-    public void log(final Object var1) {
-        try {
-            runOnMain(new Runnable() {
-                public void run() {
-                    Log.i("log", var1.toString());
-                }
-            });
-        } catch (Exception var3) {
-            var3.printStackTrace();
-        }
+    
+    public void log(final Object obj) {
+        me.log(obj);
     }
-
+    
     //位移动画
     public void moveAnimation(Object obj, String perference, float aimValue, long time, long delay) {
         ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(obj, perference, aimValue);
@@ -127,64 +121,64 @@ public abstract class BaseFragment extends Fragment {
         objectAnimator.setStartDelay(delay);
         objectAnimator.start();
     }
-
+    
     public void moveAnimation(Object obj, String perference, float aimValue, long time) {
         moveAnimation(obj, perference, aimValue, time, 0);
     }
-
+    
     public void moveAnimation(Object obj, String perference, float aimValue) {
         moveAnimation(obj, perference, aimValue, 300, 0);
     }
-
+    
     //用于进行dip和px转换
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
-
+    
     //用于进行px和dip转换
     public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
-
+    
     public boolean isNull(String s) {
         if (s == null || s.trim().isEmpty() || s.equals("null")) {
             return true;
         }
         return false;
     }
-
+    
     //更好用的跳转方式
     public boolean jump(Class<?> cls) {
         return me.jump(cls);
     }
-
+    
     //可以传任何类型参数的跳转方式
     public boolean jump(Class<?> cls, JumpParameter jumpParameter) {
         return me.jump(cls, jumpParameter);
     }
-
+    
     //带返回值的跳转
     public boolean jump(Class<?> cls, OnResponseListener onResponseListener) {
         return me.jump(cls, onResponseListener);
     }
-
+    
     //带返回值的跳转
     public boolean jump(Class<?> cls, JumpParameter jumpParameter, OnResponseListener onResponseListener) {
         return me.jump(cls, jumpParameter, onResponseListener);
     }
-
+    
     //目标Activity：设定要返回的数据
     public void setResponse(JumpParameter jumpParameter) {
         me.setResponse(jumpParameter);
     }
-
+    
     //获取跳转参数
     public JumpParameter getParameter() {
         return me.getParameter();
     }
-
+    
     //跳转动画
     public void jumpAnim(int enterAnim, int exitAnim) {
         int version = Integer.valueOf(android.os.Build.VERSION.SDK);
@@ -192,7 +186,7 @@ public abstract class BaseFragment extends Fragment {
             me.overridePendingTransition(enterAnim, exitAnim);
         }
     }
-
+    
     //权限申请
     public void requestPermission(String[] permissions, OnPermissionResponseListener onPermissionResponseListener) {
         me.requestPermission(permissions, onPermissionResponseListener);

e11a6821d9a1b2abc3c7c1083fe5a26c173dee18
kongzue <myzcxhh@live.cn>
2020-07-23 17:14:56 +0800

    6.7.5 pub

--- app/src/main/java/com/kongzue/baseframeworkdemo/activity/DemoActivity.java
+++ app/src/main/java/com/kongzue/baseframeworkdemo/activity/DemoActivity.java
@@ -1,31 +1,18 @@
 package com.kongzue.baseframeworkdemo.activity;
 
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 
 import com.kongzue.baseframework.BaseActivity;
 import com.kongzue.baseframework.BaseFragment;
-import com.kongzue.baseframework.BaseFrameworkSettings;
 import com.kongzue.baseframework.interfaces.BindView;
-import com.kongzue.baseframework.interfaces.BindViews;
 import com.kongzue.baseframework.interfaces.DarkNavigationBarTheme;
 import com.kongzue.baseframework.interfaces.DarkStatusBarTheme;
 import com.kongzue.baseframework.interfaces.FragmentLayout;
 import com.kongzue.baseframework.interfaces.Layout;
 import com.kongzue.baseframework.interfaces.NavigationBarBackgroundColor;
-import com.kongzue.baseframework.interfaces.NavigationBarBackgroundColorRes;
-import com.kongzue.baseframework.interfaces.OnBugReportListener;
 import com.kongzue.baseframework.interfaces.OnFragmentChangeListener;
 import com.kongzue.baseframework.util.FragmentChangeUtil;
 import com.kongzue.baseframework.util.JumpParameter;
-import com.kongzue.baseframework.util.Preferences;
-import com.kongzue.baseframeworkdemo.App;
 import com.kongzue.baseframeworkdemo.R;
 import com.kongzue.baseframeworkdemo.fragment.AboutFragment;
 import com.kongzue.baseframeworkdemo.fragment.FunctionFragment;
@@ -34,7 +21,6 @@ import com.kongzue.tabbar.Tab;
 import com.kongzue.tabbar.TabBarView;
 import com.kongzue.tabbar.interfaces.OnTabChangeListener;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
